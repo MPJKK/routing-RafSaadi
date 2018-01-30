fb7d00cb3be51514f10c8fc613699abb8b6f36dc
Raouf Fahandezh Saadi <raouff@ety-a2123b-21.edu.wks.metropolia.fi>
2018-01-30 10:55:22 +0200

    yooo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,36 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { AppComponent } from './app.component';
-import { TopBarComponent } from './top-bar/top-bar.component';
-import { SetupComponent } from './setup/setup.component';
-import { RoutesComponent } from './routes/routes.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule, ApplicationRef} from '@angular/core';
+import {AgmCoreModule} from '@agm/core';
+import {AppRoutingModule} from './app-routing.module';
+import {CommonModule} from '@angular/common';
+import {AppComponent} from './app.component';
+import {TopBarComponent} from './top-bar/top-bar.component';
+import {SetupComponent} from './setup/setup.component';
+import {RoutesComponent} from './routes/routes.component';
 import {HttpClientModule} from '@angular/common/http';
 import {DigitransitService} from './service/digitransit.service';
 import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TopBarComponent,
-    SetupComponent,
-    RoutesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-      HttpClientModule,
-      FormsModule
-  ],
-  providers: [DigitransitService],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TopBarComponent,
+        SetupComponent,
+        RoutesComponent
+    ],
+    imports: [
+        BrowserModule,
+        CommonModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyCQXKU0GUymCjek8hQ7qe_JlU0C5lIDH7I'
+        })
+    ],
+    providers: [DigitransitService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
